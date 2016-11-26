8633f1d14d4f85883afae7fd292e4892fd4716e8
Unknown <markowski.artur@outlook.com>
2016-11-26 01:09:32 +0100

    Ad simple main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import source.Items;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Items items = new Items("plecak", 1500, 20);
+		System.out.println(items.toString());
+	}
+
+}
