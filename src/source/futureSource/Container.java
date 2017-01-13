1e0ffebb7f79dac1f16bb44885d166e8dc377f24
artir3 <markowski.artur@outlook.com>
2017-01-13 02:39:28 +0100

    Zabawa wzorcami, testy, poprawki

---
+++ src/source/futureSource/Container.java
@@ -0,0 +1,28 @@
+package source.futureSource;
+
+
+
+import java.util.ArrayList;
+
+// TODO: 2017-01-13 Write a dybamicly added list of boxes - after interface
+class Container {
+//    private int size = 3;
+//    private Box[] conteinerList = new Box[size];
+//    private Box box = null;
+
+    public Container() {
+    }
+
+    public void createNewBox(String name){
+//        conteinerList(new Box.Builder(name).build());
+    }
+
+    // TODO: 2017-01-13
+//    public void clearBox(Box box){
+//    }
+
+    // TODO: 2017-01-13
+//    public void deleteBox(Box box){
+//    }
+
+}
